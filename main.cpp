9362bc385241642a41a2cf61262f902291304541
yharabi <yharabi@btslivh.eu>
2021-01-05 18:28:04 +0100

    Création d'un fichier main

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g1
+ *
+ * Created on 5 janvier 2021, 18:25
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout<<"Bonjour"<<endl;
+    return 0;
+}
+
